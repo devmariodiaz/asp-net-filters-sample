d9dacd355f2b2530cde553ee16304071b13a3dd5
Mario Diaz <Mario.Diaz@endava.com>
2022-08-15 14:32:11 -0500

    Adding async filter with a parameter

---
+++ MySampleAsyncActionFilterAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace filters;
+
+public class MySampleAsyncActionFilterAttribute : Attribute, IAsyncActionFilter
+{
+    private readonly string _name;
+    public MySampleAsyncActionFilterAttribute(string name)
+    {
+        _name = name;
+    }
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        Console.WriteLine($"Before Async Execution {_name}");
+        await next();
+        Console.WriteLine($"After Async Execution {_name}");
+    }
+}
